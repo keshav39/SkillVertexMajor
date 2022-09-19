3b62999267eb9218360c863898dc0b4bc0aed56c
Kar98kthakur <kartikthakur58410@gmail.com>
2022-09-19 22:52:23 +0530

    added login and register page

---
+++ includes/login-footer.php
@@ -0,0 +1,11 @@
+<!-- Footer -->
+<footer class="bg-light text-center text-lg-start">
+    <!-- Copyright -->
+    <div class="text-center p-2" id="Footer">
+        <p class="mt-3"> © 2020 Copyright:
+            <a class="text-dark fw-bold" href="#">Super-Market</a>
+        </p>
+    </div>
+    <!-- Copyright -->
+</footer>
+<!-- Footer End -->
