b2f6288d7da07e7667d5d6ce7738b0651fa113b2
Keshav Garg <kggargkg@gmail.com>
2022-10-04 00:19:53 +0530

    reset

---
+++ includes/login-footer.php
@@ -0,0 +1,18 @@
+<!-- Footer -->
+<footer id="footer" class="bg-light text-center text-lg-start">
+    <!-- Copyright -->
+    <div class="text-center p-3" id="Footer">
+        <div class="row justify-content-center">
+            <div class="col-md-12 text-center">
+                <p class="menu fw-bold p-3 pb-0">
+                    <a href="./index.php" class="text-dark pe-4">Home</a>
+                    <a href="./about.php" class="text-dark ps-4 pe-4">About</a>
+                    <a href="./contact.php" class="text-dark ps-4">Contact</a>
+                </p>
+            </div>
+        </div>
+        <p> © <?php echo date("Y"); ?> Copyright </p>
+    </div>
+    <!-- Copyright End -->
+</footer>
+<!-- Footer End -->
